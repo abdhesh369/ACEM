f9583923a9b8d0435848085dce064b55284c5039
abdhesh369 <abdheshshah111@gmail.com>
2025-02-09 23:44:35 +0545

    rewrite

--- SEM1/Labs/Lab_6/p5.c
+++ SEM1/Labs/Lab_6/p5.c
@@ -2,19 +2,31 @@
 int main()
 //void main()
 {
-    int matrix[2][2],i, j, min;
+    int n;
+    printf("enter the size of matrix:\n");
+    scanf("%d",&n);
+    int matrix[n][n],i, j, min;
 printf("enter the element of matrix\n");
-for(i=0;i<2;i++)
+for(i=0;i<n;i++)
 {
-    for(j=0;j<2;j++)
+    for(j=0;j<n;j++)
     {
     scanf("%d",&matrix[i][j]);
     }
 }
-min=matrix[0][0];
-for(i=0;i<2;i++)
+printf("matrix is:\n");
+for(i=0;i<n;i++)
 {
-    for(j=0;j<2;j++)
+    for(j=0;j<n;j++)
+    {
+    printf("%d\t",matrix[i][j]);
+    }
+    printf("\n");
+}
+min=matrix[0][0];
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
     {
         if(matrix[i][j]<=min)
         {
@@ -22,19 +34,20 @@ for(i=0;i<2;i++)
         }
     }
 }
-    for(i = 0; i < 2; i++)
+    for(i = 0; i < n; i++)
      {
         matrix[i][i] = min;        
-        matrix[i][1] = min;
+        matrix[i][i] = min;
     }
 
     printf("Modified Matrix:\n");
-    for(i = 0; i < 2; i++)
+    for(i = 0; i < n; i++)
      {
-        for(j = 0; j < 2; j++) 
+        for(j = 0; j < n; j++) 
         {
             printf("%d ", matrix[i][j]);
         }
+        printf("\n");
      }
 
  return 0;
