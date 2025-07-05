34a5c09ec6c867a9d95e5c9f673a43905bc2814d
Abdhesh Sah <abdheshshah111@gmail.com>
2025-07-05 11:46:44 +0545

    done

---
+++ Sem 2/Labs/lab5/p1.cpp
@@ -0,0 +1,16 @@
+// WAP to convert hour into object of a class Time which has minute and second as members
+#include <iostream>
+using namespace std;
+class Time
+{
+    private:
+    float hours;
+    public:
+    Time ()
+    float minute = hour*60;
+    float second = hour*3600;
+
+    Time (float hours)
+    float minute = hours*60;
+    float second = hours*3600;
+}
