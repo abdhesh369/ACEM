197f41c3a6355fc287d85346ca0f3c219ca77639
Abdhesh Sah <abdheshshah111@gmail.com>
2025-07-05 10:13:46 +0545

    Done

--- Sem 2/Labs/lab4/p3.cpp
+++ Sem 2/Labs/lab4/p3.cpp
@@ -1,3 +1,36 @@
 // WAP to overload unary minus operator using friend function.
 #include <iostream>
 using namespace std;
+class Number
+{
+    int y;
+
+public:
+    Number()
+    {
+        y = 0;
+    }
+    Number(int x)
+    {
+        y = x;
+    }
+    friend Number operator-(Number f);
+    void display();
+};
+
+Number operator-(Number f)
+{
+    f.y = -f.y;
+    return f;
+}
+
+void Number::display()
+{
+    cout << "Number after minus : " << y << endl;
+}
+int main()
+{
+    Number n(12);
+    Number result = -n;
+    result.display();
+}
