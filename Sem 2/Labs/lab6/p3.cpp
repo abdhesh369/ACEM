7ee70d6cc6de1a11057726be1d61982f5dc8595c
Abdhesh Sah <abdheshshah111@gmail.com>
2025-07-05 23:59:44 +0545

    done

---
+++ Sem 2/Labs/lab6/p3.cpp
@@ -0,0 +1,3 @@
+// Write a program to find the area of a rectangle by passing length and breadth
+// as arguments after creating member function in both derived and base class
+// with the same name.
