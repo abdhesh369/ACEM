7ab5830dc7796ab1c01fcc83f91c106ff63b93bf
Abdhesh Sah <abdheshshah111@gmail.com>
2025-09-16 10:28:34 +0545

    Done

---
+++ Sem 1/Practice 1/p10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    age = 20;
+    if (age > 18 && age < 60)
+    {
+        printf("working age");
+    }
+    else
+    {
+        printf("not working age");
+    }
+}
+    
+    
