317ba546ae69c931271c3e7325d2e780760ea5fc
Abdhesh Sah <abdheshshah111@gmail.com>
2025-09-16 10:53:00 +0545

    done

---
+++ Sem 1/Practice 1/p12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a total marks in percentage(1-100) :\n");
+    scanf("%d", &num);
+
+    if (num >= 80)
+    {
+        printf("Distinction");
+    }
+    else if (num >= 40)
+    {
+        printf("Pass");
+    }
+
+    else
+    {
+        printf("Fail");
+    }
+}
