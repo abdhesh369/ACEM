84c5a3bfbfee165205e65b36dc5ab66e1779be6d
Abdhesh Sah <abdheshshah111@gmail.com>
2025-09-18 18:51:05 +0545

    aaa

--- Sem 1/Practice 1/p32.c
+++ Sem 1/Practice 1/p32.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
-int main() {
-    int arr[2][2];
-    
-    printf("Enter 4 numbers:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+int main()
+{
+    int arr[2][3];
+
+    printf("Enter numbers:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             scanf("%d", &arr[i][j]);
         }
     }
 
     printf("Matrix:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             printf("%d ", arr[i][j]);
         }
-        printf("\n"); 
+        printf("\n");
+    }
+    printf("Matrix transposed:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d ", arr[j][i]);
+        }
+        printf("\n");
     }
 
     return 0;
