3710a2aae92af387207bca8878f61ee264bbe81c
Abdhesh Sah <abdheshshah111@gmail.com>
2025-09-20 19:04:41 +0545

    46

--- Sem 1/Practice/p46.c
+++ Sem 1/Practice/p46.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 int main()
 {
     char sentance[99];
     char sentance1[99];
     printf("enter your sentance:\n");
     fgets(sentance, sizeof(sentance), stdin);
-    sentance[strcspn(sentance,"\n")]='\0';
-    printf("You entered:\n%s\n", sentance);
+    sentance[strcspn(sentance, "\n")] = '\0';
+    for (int i = 0; i < strlen(sentance); i++)
+    {
+
+        if (sentance[i] == strrev(sentance[i]))
+        {
+            printf("it is pelindrome");
+        }
+        else
+        {
+            printf("it is not pelindrome");
+        }
+    }
+
     return 0;
 }
