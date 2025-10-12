b6fca512d3914a8e110e22fbe8e5e4e2c426a9d2
Abdhesh Sah <abdheshshah111@gmail.com>
2025-10-12 12:51:32 +0545

    done

--- Sem 2/Tutorials/Tutorial 2/p4.cpp
+++ Sem 2/Tutorials/Tutorial 2/p4.cpp
@@ -1,1 +1,33 @@
 // Write a program to overload + operator to concatenate two strings.
+#include <iostream>
+using namespace std;
+class sentance
+{
+    string word;
+
+public:
+    sentance(string s = "")
+    {
+        word = s;
+    }
+    sentance operator+(const sentance &d)
+    {
+        sentance temp;
+        temp.word = word + d.word;
+        return temp;
+    }
+    void display()
+    {
+        cout << "Concatenated String: " << word << endl;
+    }
+};
+int main()
+{
+    sentance s1("Abdhesh ");
+    sentance s2("sah ");
+    sentance s3;
+    s3 = s1 + s2;
+    s3.display();
+
+    return 0;
+}
