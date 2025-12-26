fd695a316aa5feef256b203ac43bf7ec3854780a
Abdhesh <abdheshshah111@gmail.com>
2025-12-26 14:59:17 +0545

    done

--- Sem 1/Practice/p71.c
+++ Sem 1/Practice/p71.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num1, num2;
+    char ch;
+    // clrscr();
+    printf("enter number: ");
+    scanf("%d%d", &num1, &num2);
+    printf("enter operator:\n");
+    scanf(" %c", &ch);
+    switch (ch)
+    {
+    case '+':
+        printf("the sum is %d", num1 + num2);
 
-// File: p71.c
+        break;
 
-int main() {
-    printf("This is p71.c\n");
+    case '-':
+        printf("the difference is %d", num1 - num2);
+
+        break;
+
+    case '*':
+        printf("the multiply is %d", num1 * num2);
+
+        break;
+
+    case '/':
+        printf("the division is %f", (float)num1 / num2);
+
+        break;
+    default:
+        printf("invalid operator");
+    }
+    getch();
     return 0;
 }
